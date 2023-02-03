3d1d4120561f86ebc6cae7e4ad9091e52cc321ad
virginia-zuror <virginiazuor@gmail.com>
2023-02-03 01:14:06 +0100

    html card

--- pages/PokeAPI/PokeAPI.js
+++ pages/PokeAPI/PokeAPI.js
@@ -4,7 +4,15 @@ let allPokemons=[];
 
 const Template = () =>`
 <div id="poke-container">
-<section id="cards"></section>
+<div id="searcher">
+<input type="text" id="inputName">
+<button id="searchBtn">Search</button>
+</div>
+<div id="selectorType">
+<ul class="TypeList"></ul>
+</div>
+<section id="cards">
+</section>
 </div>`
 
 
@@ -24,7 +32,11 @@ const mapPokemons = (allPokemons) =>{
     allCharactersPoke = allPokemons.map((pokemon) =>({
         name : pokemon.name,
         experience : pokemon.base_experience,
+        ability : pokemon.abilities[0].ability.name,
+        type : pokemon.types[0].type.name,
+              
     }));
+    
     
     printPokemons(allCharactersPoke);
 };
@@ -32,27 +44,52 @@ const mapPokemons = (allPokemons) =>{
   
 
 const printPokemons =(pokemons)=>{
-
+    let number= 1;
     for (const pokemon of allCharactersPoke) {
+        
         const figure = document.createElement("figure");
-        const name = document.createTextNode(` Name: ${pokemon.name}`)
-        const exp = document.createTextNode(`Exp points: ${pokemon.experience}`)
-        figure.appendChild(name);
-        figure.appendChild(exp);
+        figure.setAttribute("id", `${pokemon.name}`)
+        figure.innerHTML=`
+        <h1>${pokemon.name}</h1>
+        <h2>Puntos de exp.: ${pokemon.experience}</h2>
+        <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png" alt= "${pokemon.name} image"/>
+        <p>Habilidad: ${pokemon.ability}</p>`;
+      
         document.querySelector("#cards").appendChild(figure);
 
+        number++;
     }
 }
+
+const filterByName =()=>{//pendiente no encuentra
+    const inputName = document.querySelector("#inputName");
+    const cards = document.querySelector("#cards");
+    const idCard = document.querySelectorAll("figure");
+    for (const pokemon of allCharactersPoke) {
+        const filteredPokemons = allCharactersPoke.filter(pokemon =>{
+            pokemon.name.includes(inputName.value)? cards.innerHTML = idCard[pokemon] : cards.innerHTML = `No hemos encontrado ninguna coincidencia`;
+    });
+    
+};
+}
+
+    
+
+
+
 
 const addListeners =()=>{
     document.querySelector("#start-btn")
     .addEventListener("click", ()=>{
         initContent("Hub");
     });
+    document.querySelector("#searchBtn")
+    .addEventListener("click", ()=> filterByName(allCharactersPoke));
 }
  
 export const printTemplate = ()=>{
     document.querySelector("#dashboard").innerHTML = Template();
     getPokemons();
     addListeners();
+    
 } 
