f46bb332a996bb365709f7184322704cc363a4dd
virginia-zuror <virginiazuor@gmail.com>
2023-02-06 13:26:39 +0100

    rolldice

--- pages/RollDice/RollDice.js
+++ pages/RollDice/RollDice.js
@@ -5,30 +5,45 @@ import { printTemplate as navButtonsTemplate } from "../../components/navbuttons
 const Template =()=>`
 <div id="main_container">
     <div id="dice_container">
-        <div id="dice_machine"></div>
-        <div id="dice_player"></div>
+        <div id="machine"><h2>Ordenador</h2><div id="dice_machine"></div></div>
+        <div id="player"><h2>${localStorage.getItem(["user"])}</h2><div id="dice_player"></div></div>
     </div>
     <div id="chooseNumber">
 
     </div>
     <button id="throw">Lanza el dado</button>
-    <div id="result"></div>
+    <div id="resultGame"></div>
 
 
 </div>
 `
 let posibilities = 6;
 const diceResult =(posibilities)=>{
-let result = Math.floor(Math.random()*posibilities);
-console.log(result);
+    document.querySelector("#resultGame").innerHTML = "";
+//el jugador
+    let resultPlayer = Math.floor(Math.random(1,posibilities)*posibilities);
+document.querySelector("#dice_player").innerHTML = resultPlayer;
+//la maquina
+    let resultMachine = Math.floor(Math.random(1,posibilities)*posibilities);
+document.querySelector("#dice_machine").innerHTML= resultMachine;
+
+if(resultMachine > resultPlayer){
+    document.querySelector("#resultGame").innerHTML = `Ha ganado la máquina, pronto gobernaré el mundo.`;
+  }else if(resultMachine == resultPlayer){
+    document.querySelector("#resultGame").innerHTML = `Empate, ni pa tí ni pa mí.`;
+  }else{
+    document.querySelector("#resultGame").innerHTML = `Vaya, me has ganado, pondremos tu nombre a una calle.`
+  }
+
 }
 
 
-const buttonDice = document.querySelector("#throw");
 
 
 const addListenersRoll =()=>{
-buttonDice.addEventListener("click", ()=> diceResult())
+const buttonDice = document.querySelector("#throw");
+
+buttonDice.addEventListener("click", ()=> diceResult(posibilities))
 }
 
 export const printTemplate =()=>{
